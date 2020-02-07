ef8d5e6658d11628a6e0b2fe4ae5d0d3cfac263f
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-02-07 13:07:05 -0300

    Rotas e componente de rota nao encontrada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
 import { Component } from "@angular/core";
-import { PhotoService } from "./photos/photo/photo.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  photos: Object[] = [];
-
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService.listFromUser("flavio").subscribe(photos => {
-      console.log(photos[0].description);
-      this.photos = photos;
-    });
-  }
-}
+export class AppComponent {}
